4f1de2a32eb102f9c49d61d4cb670794c577cc09
Simos Xenitellis <simos@gnome.org>
2010-04-12 01:23:35 +0300

    Fix Preferences extension ID.

--- defaults/preferences/export_all_certificates.js
+++ defaults/preferences/export_all_certificates.js
@@ -35,4 +35,4 @@
  * ***** END LICENSE BLOCK ***** */
 
 // See http://kb.mozillazine.org/Localize_extension_descriptions
-pref("extensions.simos.list.s@googlemail.com.description", "chrome://export_all_certificates/locale/export_all_certificates.properties");
+pref("extensions.ExportAllCertificates@simos.info.description", "chrome://export_all_certificates/locale/export_all_certificates.properties");
