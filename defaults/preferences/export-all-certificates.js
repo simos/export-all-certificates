7a462b80ed542a59130e20146d7ff47726b04a4d
Simos Xenitellis <simos@gnome.org>
2011-02-04 22:47:45 +0200

    Additional update to Firefox 4

---
+++ defaults/preferences/export-all-certificates.js
@@ -0,0 +1,2 @@
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.ExportAllCertificates@simos.info.description", "chrome://export-all-certificates/locale/export-all-certificates.properties");
