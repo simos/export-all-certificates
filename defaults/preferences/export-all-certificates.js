9e7e132048444dfff0106eafa29fe77a0dd6fd05
Simos Xenitellis <simos@gnome.org>
2011-03-26 21:17:25 +0200

    Updated filename

--- defaults/preferences/export-all-certificates.js
+++
@@ -1,2 +0,0 @@
-// https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.ExportAllCertificates@simos.info.description", "chrome://export-all-certificates/locale/export-all-certificates.properties");
---
+++ defaults/preferences/exportallcertificates.js
@@ -0,0 +1,2 @@
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.ExportAllCertificates@simos.info.description", "chrome://exportallcertificates/locale/exportallcertificates.properties");
